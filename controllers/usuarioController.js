e19026cbdf83f3fc1a0f87616e5edb257fbd5e07
Nar <dev@webdevelop.dev>
2021-04-09 21:23:31 +0200

    Agregando validacion a la API

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,7 +1,17 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
 
 
 exports.crearUsuario = async (req, res) => {
+
+   //Revisar si hay errores
+   const errores = validationResult(req);
+   if (!errores.isEmpty()) {
+      return res.status(400).json({ errores: errores.array() })
+   }
 
    //Extraer email y password
    const { email, password } = req.body
@@ -17,11 +27,30 @@ exports.crearUsuario = async (req, res) => {
       //crear el nuevo usuario
       usuario = new Usuario(req.body);
 
+      //Hashear el password
+      const salt = await bcryptjs.genSalt(10);
+      usuario.password = await bcryptjs.hash(password, salt);
+
       //Guardar el usuario
       await usuario.save();
 
-      //Mensaje de confirmación 
-      res.json({ msg: 'Usuario creado correctamente' });
+      //guardar y firmat el JWT
+      const payload = {
+         usuario: {
+            id: usuario.id
+         }
+      };
+
+      //firmar el JSON WEB TOKEN 
+      jwt.sign(payload, process.env.SECRETA, {
+         expiresIn: 3_600
+      }, (error, token) => {
+         if (error) throw error;
+
+         //Mensaje de confirmación
+         res.json({ token });
+      });
+
    } catch (error) {
       console.log(error);
       res.status(400).send('Hubo un error');
