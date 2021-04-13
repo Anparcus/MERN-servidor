ddd4ab0a1e0bd9dfd721098cae8dee47a40389ce
Nar <dev@webdevelop.dev>
2021-04-13 15:00:53 +0200

    Actualizar Proyectos con PUT

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -14,10 +14,20 @@ router.post('/',
    proyectoController.crearProyecto
 
 );
+
+//Obtener todos los proyectos
 router.get('/',
    auth,
    proyectoController.obtenerProyectos
+)
 
+//Actualizar proyectos via ID
+router.put('/:id',
+   auth,
+   [
+      check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+   ],
+   proyectoController.actualizarProyecto
 );
 
 module.exports = router;
